ad7d1efdad844e6b7e6c4fc342885aea78275128
Daniel Bohlin <daniel.bohlin@gmail.com>
2023-07-31 13:18:01 +0200

    prepare interface for readonly

--- Tharga.MongoDB/IRepositoryCollection.cs
+++ Tharga.MongoDB/IRepositoryCollection.cs
@@ -7,13 +7,17 @@ using MongoDB.Driver;
 
 namespace Tharga.MongoDB;
 
-public interface IRepositoryCollection
+public interface IReadOnlyRepositoryCollection
 {
-    Task DropCollectionAsync();
     Task<long> GetSizeAsync();
 }
 
-public interface IRepositoryCollection<TEntity, TKey> : IRepositoryCollection
+public interface IRepositoryCollection : IReadOnlyRepositoryCollection
+{
+    Task DropCollectionAsync();
+}
+
+public interface IReadOnlyRepositoryCollection<TEntity, TKey> : IReadOnlyRepositoryCollection
     where TEntity : EntityBase<TKey>
 {
     IAsyncEnumerable<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate = null, Options<TEntity> options = null, CancellationToken cancellationToken = default);
@@ -24,6 +28,13 @@ public interface IRepositoryCollection<TEntity, TKey> : IRepositoryCollection
     Task<TEntity> GetOneAsync(Expression<Func<TEntity, bool>> predicate = null, OneOption<TEntity> options = null, CancellationToken cancellationToken = default);
     Task<TEntity> GetOneAsync(FilterDefinition<TEntity> filter, OneOption<TEntity> options = null, CancellationToken cancellationToken = default);
     Task<T> GetOneAsync<T>(Expression<Func<T, bool>> predicate = null, OneOption<T> options = null, CancellationToken cancellationToken = default) where T : TEntity;
+    Task<long> CountAsync(Expression<Func<TEntity, bool>> predicate);
+    Task<long> CountAsync(FilterDefinition<TEntity> filter);
+}
+
+public interface IRepositoryCollection<TEntity, TKey> : IReadOnlyRepositoryCollection<TEntity, TKey>, IRepositoryCollection
+    where TEntity : EntityBase<TKey>
+{
     Task<bool> AddAsync(TEntity entity);
     Task AddManyAsync(IEnumerable<TEntity> entities);
     Task<EntityChangeResult<TEntity>> AddOrReplaceAsync(TEntity entity);
@@ -32,6 +43,4 @@ public interface IRepositoryCollection<TEntity, TKey> : IRepositoryCollection
     Task<TEntity> DeleteOneAsync(TKey id);
     Task<TEntity> DeleteOneAsync(Expression<Func<TEntity, bool>> predicate = null, FindOneAndDeleteOptions<TEntity, TEntity> options = default);
     Task<long> DeleteManyAsync(Expression<Func<TEntity, bool>> predicate);
-    Task<long> CountAsync(Expression<Func<TEntity, bool>> predicate);
-    Task<long> CountAsync(FilterDefinition<TEntity> filter);
 }
