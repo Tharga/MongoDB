5ff1d1bdc81785c2466cc00e8a8a5c22cb13707e
Daniel Bohlin <daniel.bohlin@live.se>
2024-08-10 09:09:13 +0200

    start implement entity scope

--- Tharga.MongoDB/Lockable/LockableRepositoryCollectionBase.cs
+++ Tharga.MongoDB/Lockable/LockableRepositoryCollectionBase.cs
@@ -4,6 +4,7 @@ using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using MongoDB.Bson;
 using MongoDB.Driver;
 using Tharga.MongoDB.Disk;
 
@@ -41,8 +42,8 @@ public class LockableRepositoryCollectionBase<TEntity, TKey> : DiskRepositoryCol
 
     public override Task<EntityChangeResult<TEntity>> AddOrReplaceAsync(TEntity entity)
     {
-        //TODO: Do not for locked entities
-        throw new NotImplementedException();
+        //TODO: Check if an entity can be added. But do not allow updates.
+        throw new NotSupportedException($"Use {nameof(PickForUpdate)} to get an update {nameof(EntityScope<TEntity, TKey>)} that can be used for update.");
     }
 
     public override Task<EntityChangeResult<TEntity>> ReplaceOneAsync(TEntity entity, OneOption<TEntity> options = null)
@@ -96,6 +97,55 @@ public class LockableRepositoryCollectionBase<TEntity, TKey> : DiskRepositoryCol
     public override Task<long> DeleteManyAsync(Expression<Func<TEntity, bool>> predicate)
     {
         //TODO: Do not for locked entities
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<EntityScope<TEntity, TKey>> PickForUpdate(ObjectId documentId, TimeSpan? timeout, string actor)
+    {
+        //var filter = Builders<Entities.DistillerDocument>.Filter.And(
+        //    Builders<Entities.DistillerDocument>.Filter.Eq(x => x.Id, documentId),
+        //    Builders<Entities.DistillerDocument>.Filter.Eq(x => x.DocumentLock, null));
+
+        //var infoTime = DateTime.UtcNow;
+        //var documentLock = new Entities.DocumentLock { LockKey = Guid.NewGuid(), InfoTime = infoTime, Actor = actor.Name, ExceptionInfo = null, ExpireTime = infoTime.Add(timeout) };
+        //var update = new UpdateDefinitionBuilder<Entities.DistillerDocument>().Set(x => x.DocumentLock, documentLock);
+
+        //var collection = GetDocumentRepositoryCollection(pipelineContext);
+        //var result = await collection.UpdateOneAsync(filter, update);
+        //if (result.Before == null)
+        //{
+        //    //Document is missing or is already locked
+        //    var doc = await collection.GetOneAsync(x => x.Id == documentId);
+        //    if (doc == null) return null;
+        //    if (doc.DocumentLock?.ExceptionInfo != null) return null;
+
+        //    throw new NotImplementedException($"Retry '{nameof(PickForUpdate)}' has not yet been implemented.");
+        //}
+
+        //return new EntityScope<Entities.DistillerDocument>(result.Before, (d, exception) =>
+        //{
+        //    try
+        //    {
+        //        return ReleaseAsync(pipelineContext, d, exception, documentLock, false, false);
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Debugger.Break();
+        //        var doc = d with
+        //        {
+        //            DocumentLock = new DocumentLock { LockKey = Guid.NewGuid(), InfoTime = DateTime.UtcNow, Actor = actor.Name, ExceptionInfo = new DocumentLock.ExceptionData { Type = e.GetType().Name, Message = e.Message, StackTrace = e.StackTrace }, ExpireTime = infoTime.Add(timeout) }
+        //        };
+        //        collection.ReplaceOneAsync(doc);
+        //        _logger.LogError(e, e.Message);
+        //        return Task.FromResult(false);
+        //    }
+        //});
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<EntityScope<TEntity, TKey>> WaitForUpdate(ObjectId documentId, TimeSpan? timeout = default, string actor = default)
+    {
+        //TODO: Wait for the entity to be released, then take a lock for the item to be updated.
         throw new NotImplementedException();
     }
 }
