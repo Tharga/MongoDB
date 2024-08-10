e852bea8c0eb143beb8a579840c904e68f86427d
Daniel Bohlin <daniel.bohlin@live.se>
2024-08-10 08:22:59 +0200

    create framework

---
+++ Tharga.MongoDB/Lockable/Class1.cs
@@ -0,0 +1,227 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
+
+namespace Tharga.MongoDB.Lockable;
+
+public abstract record LockableEntityBase<TKey> : EntityBase<TKey>
+{
+    [BsonIgnoreIfNull]
+    public DocumentLock DocumentLock { get; internal init; }
+
+    [BsonIgnoreIfDefault]
+    public int UnlockCounter { get; internal init; }
+}
+
+public record DocumentLock
+{
+    public Guid LockKey { get; internal init; }
+    public DateTime LockTime { get; internal init; }
+
+    [BsonIgnoreIfDefault]
+    public DateTime? ExpireTime { get; internal init; }
+
+    [BsonIgnoreIfDefault]
+    public string Actor { get; internal init; }
+
+    [BsonIgnoreIfDefault]
+    public Exception Exception { get; internal init; }
+}
+
+public interface ILockableRepositoryCollection<TEntity, TKey> : IRepositoryCollection<TEntity, TKey>
+    where TEntity : LockableEntityBase<TKey>
+{
+}
+
+public class LockableRepositoryCollectionBase<TEntity, TKey> : RepositoryCollectionBase<TEntity, TKey>, ILockableRepositoryCollection<TEntity, TKey>
+    where TEntity : LockableEntityBase<TKey>
+{
+    /// <summary>
+    /// Override this constructor for static collections.
+    /// </summary>
+    /// <param name="mongoDbServiceFactory"></param>
+    /// <param name="logger"></param>
+    protected LockableRepositoryCollectionBase(IMongoDbServiceFactory mongoDbServiceFactory, ILogger<RepositoryCollectionBase<TEntity, TKey>> logger = null)
+        : base(mongoDbServiceFactory, logger)
+    {
+    }
+
+    /// <summary>
+    /// Use this constructor for dynamic collections together with ICollectionProvider.
+    /// </summary>
+    /// <param name="mongoDbServiceFactory"></param>
+    /// <param name="logger"></param>
+    /// <param name="databaseContext"></param>
+    protected LockableRepositoryCollectionBase(IMongoDbServiceFactory mongoDbServiceFactory, ILogger<RepositoryCollectionBase<TEntity, TKey>> logger, DatabaseContext databaseContext)
+        : base(mongoDbServiceFactory, logger, databaseContext)
+    {
+    }
+
+    public override IAsyncEnumerable<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate = null, Options<TEntity> options = null,
+        CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override IAsyncEnumerable<TEntity> GetAsync(FilterDefinition<TEntity> filter, Options<TEntity> options = null,
+        CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override IAsyncEnumerable<T> GetAsync<T>(Expression<Func<T, bool>> predicate = null, Options<T> options = null,
+        CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override IAsyncEnumerable<T> GetProjectionAsync<T>(Expression<Func<T, bool>> predicate = null, Options<T> options = null,
+        CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<Result<TEntity, TKey>> QueryAsync(Expression<Func<TEntity, bool>> predicate = null, Options<TEntity> options = null, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<Result<TEntity, TKey>> QueryAsync(FilterDefinition<TEntity> filter, Options<TEntity> options = null, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override IAsyncEnumerable<ResultPage<TEntity, TKey>> GetPagesAsync(Expression<Func<TEntity, bool>> predicate = null, Options<TEntity> options = null,
+        CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<TEntity> GetOneAsync(TKey id, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<TEntity> GetOneAsync(Expression<Func<TEntity, bool>> predicate = null, OneOption<TEntity> options = null, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<TEntity> GetOneAsync(FilterDefinition<TEntity> filter, OneOption<TEntity> options = null, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<T> GetOneAsync<T>(Expression<Func<T, bool>> predicate = null, OneOption<T> options = null,
+        CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<T> GetOneProjectionAsync<T>(Expression<Func<T, bool>> predicate = null, OneOption<T> options = null,
+        CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<long> CountAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<long> CountAsync(FilterDefinition<TEntity> filter, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override IAsyncEnumerable<TTarget> AggregateAsync<TTarget>(FilterDefinition<TEntity> filter, EPrecision precision, AggregateOperations<TTarget> operations,
+        CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<long> GetSizeAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task AddAsync(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<bool> TryAddAsync(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task AddManyAsync(IEnumerable<TEntity> entities)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<EntityChangeResult<TEntity>> AddOrReplaceAsync(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<EntityChangeResult<TEntity>> ReplaceOneAsync(TEntity entity, OneOption<TEntity> options = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<long> UpdateAsync(FilterDefinition<TEntity> filter, UpdateDefinition<TEntity> update)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<EntityChangeResult<TEntity>> UpdateOneAsync(TKey id, UpdateDefinition<TEntity> update)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<EntityChangeResult<TEntity>> UpdateOneAsync(FilterDefinition<TEntity> filter, UpdateDefinition<TEntity> update, FindOneAndUpdateOptions<TEntity> options)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<EntityChangeResult<TEntity>> UpdateOneAsync(FilterDefinition<TEntity> filter, UpdateDefinition<TEntity> update, OneOption<TEntity> options = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<TEntity> DeleteOneAsync(TKey id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<TEntity> DeleteOneAsync(Expression<Func<TEntity, bool>> predicate, FindOneAndDeleteOptions<TEntity, TEntity> options)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<TEntity> DeleteOneAsync(Expression<Func<TEntity, bool>> predicate = null, OneOption<TEntity> options = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<long> DeleteManyAsync(Expression<Func<TEntity, bool>> predicate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override IMongoCollection<TEntity> GetCollection()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task DropCollectionAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
