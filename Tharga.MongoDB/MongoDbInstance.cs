ece0901b56b9ce5a882a1c8b654f3f404d51821b
Daniel Bohlin <daniel.bohlin@live.se>
2025-07-22 20:19:28 +0200

    update registration

---
+++ Tharga.MongoDB/MongoDbInstance.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace Tharga.MongoDB;
+
+internal class MongoDbInstance : IMongoDbInstance
+{
+    public readonly ConcurrentDictionary<Type, Type> RegisteredRepositories = new();
+    public readonly ConcurrentDictionary<Type, Type> RegisteredCollections = new();
+}
