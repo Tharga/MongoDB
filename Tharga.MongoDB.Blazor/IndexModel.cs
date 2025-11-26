755ccea8e0959c9f532daad8fa919939180919c8
Daniel Bohlin <daniel.bohlin@live.se>
2025-11-26 02:59:33 +0100

    show calls

---
+++ Tharga.MongoDB.Blazor/IndexModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Tharga.MongoDB.Blazor;
+
+public record IndexModel
+{
+    public required string Name { get; init; }
+    public required IndexMeta Current { get; init; }
+    public required IndexMeta Defined { get; init; }
+    public required bool EqualFields { get; init; }
+}
